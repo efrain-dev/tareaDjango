331eabc4570b8dde7e6a4bd5844425c57005c1c8
Efrain de Leon <efraindeleon12@outlook.com>
2020-05-26 22:18:03 -0600

    Primer commit

---
+++ tareaDjango/urls.py
@@ -0,0 +1,28 @@
+"""tareaDjango URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from tareaDjango.views import formulario1,formulario2,formulario3,formulario4,formulario5,home
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', home),
+    path('form1/', formulario1),
+    path('form2/', formulario2),
+    path('form3/', formulario3),
+    path('form4/', formulario4),
+    path('form5/', formulario5),
+]
